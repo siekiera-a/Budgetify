be14c1ed4f68859d1239e46bcf674b7687a23483
siekiera-a <siekiera.a31@gmail.com>
2021-08-23 23:18:24 +0200

    Initial commit

---
+++ src/main/java/pl/siekiera/budgetify/BudgetifyApplication.java
@@ -0,0 +1,15 @@
+package pl.siekiera.budgetify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.nio.charset.StandardCharsets;
+
+@SpringBootApplication
+public class BudgetifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/siekiera/budgetify/BudgetifyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.siekiera.budgetify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
