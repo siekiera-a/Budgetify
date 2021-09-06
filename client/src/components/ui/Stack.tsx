270dfaa62108eea4c65cd2a6dbdf47f71370ba1a
siekiera-a <siekiera.a31@gmail.com>
2021-09-06 23:44:10 +0200

    feat(client): stack component

---
+++ client/src/components/ui/Stack.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { OpaqueColorValue, View, ViewProps } from "react-native";
+import { useList } from "../../hooks/useList";
+import { Divider } from "react-native-paper";
+
+interface Props extends ViewProps {
+  children: React.ReactNode;
+  space: number;
+  direction?: "column" | "column-reverse";
+  withSeparator?: boolean;
+  separatorColor?: string | OpaqueColorValue;
+}
+
+export function Stack({
+  children,
+  space,
+  direction = "column",
+  withSeparator = false,
+  separatorColor,
+  ...rest
+}: Props) {
+  const elements = React.Children.toArray(children);
+  const list = useList(elements);
+
+  return (
+    <View
+      style={{
+        flexDirection: direction,
+      }}
+      {...rest}
+    >
+      {list.map(([child, { isLast }], index) => {
+        if (isLast) {
+          return child;
+        }
+        return (
+          <React.Fragment key={index}>
+            {child}
+            {space > 0 && <View style={{ width: space, height: space }} />}
+            {withSeparator && (
+              <Divider style={{ backgroundColor: separatorColor }} />
+            )}
+          </React.Fragment>
+        );
+      })}
+    </View>
+  );
+}
