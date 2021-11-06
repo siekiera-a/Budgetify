7cc6a4fbf6bd7883dc4ceeb2accca40a18c94745
siekiera-a <siekiera.a31@gmail.com>
2021-11-06 19:10:17 +0100

    fix(components): close group creator when group created

--- client/src/components/Groups/Creator/GroupCreator.tsx
+++ client/src/components/Groups/Creator/GroupCreator.tsx
@@ -1,11 +1,12 @@
+import { useNavigation } from "@react-navigation/core";
 import { createStackNavigator } from "@react-navigation/stack";
 import { useInterpret, useSelector } from "@xstate/react";
 import React from "react";
 import { useHttp, useSettings } from "../../../contexts";
-import { SearchPeople } from "./SearchPeople";
 import { CreateGroup } from "./CreateGroup";
 import { createGroupMachine } from "./machines/groupMachine";
 import { SearchUsersActor } from "./machines/searchUsersMachine";
+import { SearchPeople } from "./SearchPeople";
 
 export type CreateGroupStackNavigatorParams = {
   Group: undefined;
@@ -17,11 +18,14 @@ const Stack = createStackNavigator<CreateGroupStackNavigatorParams>();
 export function GroupCreator() {
   const { client } = useHttp();
   const { dictionary } = useSettings();
+  const { goBack } = useNavigation();
   const service = useInterpret(createGroupMachine(client));
   const searchService = useSelector(
     service,
     ({ context }) => context.searchUsersService
   );
+  
+  service.onDone(goBack);
 
   return (
     <Stack.Navigator screenOptions={{ headerTitleAlign: "center" }}>
