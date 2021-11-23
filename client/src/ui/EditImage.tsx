c6683787183065efc55ed30a4bd1a3fb46dc2ec9
siekiera-a <siekiera.a31@gmail.com>
2021-11-24 00:03:45 +0100

    feat(ui): add press action to edit image

--- client/src/ui/EditImage.tsx
+++ client/src/ui/EditImage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Avatar as PaperAvatar } from "react-native-paper";
 import { AvatarComponent as Avatar } from "./Avatar";
-import { StyleSheet, View, ViewProps } from "react-native";
+import { StyleSheet, TouchableHighlight, View, ViewProps } from "react-native";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
 type Props = {
@@ -10,6 +10,7 @@ type Props = {
   backgroundColor?: string;
   color?: string;
   borderColor?: string;
+  onPress?: () => void;
 } & Pick<ViewProps, "style">;
 
 export function EditImage({
@@ -19,26 +20,31 @@ export function EditImage({
   color,
   style,
   borderColor,
+  onPress,
 }: Props) {
   const styles = makeStyles({ backgroundColor, borderColor });
 
   return (
     <View style={[style, styles.wrapper]}>
-      {image ? (
-        <Avatar avatar={image} name="" size={size} />
-      ) : (
-        <PaperAvatar.Icon
-          icon="camera-off"
-          size={size}
-          style={styles.icon}
-          color={color}
-        />
-      )}
-      <PaperAvatar.Icon
-        icon={(props) => <MaterialIcons name="edit" {...props} />}
-        size={size / 3.5}
-        style={styles.smallIcon}
-      />
+      <TouchableHighlight onPress={onPress} style={[styles.roundedWrapper]}>
+        <View style={styles.roundedWrapper}>
+          {image ? (
+            <Avatar avatar={image} name="" size={size} />
+          ) : (
+            <PaperAvatar.Icon
+              icon="camera-off"
+              size={size}
+              style={styles.icon}
+              color={color}
+            />
+          )}
+          <PaperAvatar.Icon
+            icon={(props) => <MaterialIcons name="edit" {...props} />}
+            size={size / 3.5}
+            style={styles.smallIcon}
+          />
+        </View>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -62,6 +68,9 @@ const makeStyles = ({ borderColor, backgroundColor }: StyleProps) => {
       borderRadius: 50,
       borderColor,
       backgroundColor,
+    },
+    roundedWrapper: {
+      borderRadius: 50,
     },
   });
 };
