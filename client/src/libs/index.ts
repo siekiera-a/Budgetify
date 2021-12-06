f4a67d93f290d90e9edcec0153089055c441aca4
siekiera-a <siekiera.a31@gmail.com>
2021-12-07 00:23:51 +0100

    feat(utils): format date, format price

--- client/src/libs/index.ts
+++ client/src/libs/index.ts
@@ -3,5 +3,7 @@ export { HttpClient } from "./http/HttpClient";
 export { Data, IError, HttpOptions, ContentType } from "./http/types";
 
 export { convertImageToSend } from "./utils/convertImageToSend";
+export { formatPrice } from "./utils/formatPrice";
+export { formatDate } from "./utils/formatDate";
 
 export * from "./api";
