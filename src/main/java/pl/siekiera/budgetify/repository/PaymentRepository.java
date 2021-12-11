74f8534dc2a9ed8199edc2559817a6e618582280
siekiera-a <siekiera.a31@gmail.com>
2021-12-11 23:39:45 +0100

    fix(server): return payments without duplicates

--- src/main/java/pl/siekiera/budgetify/repository/PaymentRepository.java
+++ src/main/java/pl/siekiera/budgetify/repository/PaymentRepository.java
@@ -8,7 +8,6 @@ import pl.siekiera.budgetify.entity.InvoiceEntity;
 import pl.siekiera.budgetify.entity.PaymentEntity;
 import pl.siekiera.budgetify.entity.UserEntity;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -18,7 +17,7 @@ public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
     @Query("select p from PaymentEntity p join fetch p.paymentHistory as ph " +
         "join fetch ph.status join fetch p.user where p.invoice = " +
         ":invoice")
-    List<PaymentEntity> findInvoicePayments(@Param("invoice") InvoiceEntity invoice);
+    Set<PaymentEntity> findInvoicePayments(@Param("invoice") InvoiceEntity invoice);
 
     @Query("select p from PaymentEntity p join fetch p.invoice i join fetch i.user join fetch i" +
         ".group join fetch p.paymentHistory ph join fetch ph.status where p.user = :user and p" +
@@ -26,10 +25,13 @@ public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
     Set<PaymentEntity> findUserReceivables(@Param("user") UserEntity user);
 
     @Query("select p from PaymentEntity p join fetch p.invoice i join fetch p.user join fetch i" +
-        ".group join fetch p.paymentHistory ph join fetch ph.status where i.user = :user and p.user <> :user")
+        ".group join fetch p.paymentHistory ph join fetch ph.status where i.user = :user and p" +
+        ".user <> :user")
     Set<PaymentEntity> findPaymentForSettlement(@Param("user") UserEntity user);
 
-    @Query("select p from PaymentEntity p join fetch p.paymentHistory ph join fetch ph.status where p.id = :id and p.user = :user")
-    Optional<PaymentEntity> findUserPaymentToPay(@Param("id") long paymentId, @Param("user") UserEntity user);
+    @Query("select p from PaymentEntity p join fetch p.paymentHistory ph join fetch ph.status " +
+        "where p.id = :id and p.user = :user")
+    Optional<PaymentEntity> findUserPaymentToPay(@Param("id") long paymentId,
+                                                 @Param("user") UserEntity user);
 
 }
